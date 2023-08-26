140264f3dbb01a2faef3215ba4ffc211c474d4e2
madyanes <madyanes.dev@gmail.com>
2023-08-26 07:53:33 +0700

    initial commit

---
+++ 1 two-sum/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  const numMap = {};
+
+  for (let i = 0; i < nums.length; i += 1) {
+    const complement = target - nums[i];
+
+    if (complement in numMap) {
+      return [numMap[complement], i];
+    }
+
+    numMap[nums[i]] = i;
+  }
+
+  return [];
+};
+
+export default { twoSum };
